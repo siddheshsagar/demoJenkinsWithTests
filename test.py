805c77a29f954995aaa530a26f54201b6672526e
Siddhesh Sagar <77565933+siddheshsagar@users.noreply.github.com>
2023-03-30 10:27:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from ops import *
 
-def test_addition():
-  assert addition(4,6) == 10
+def test_add():
+  assert add(4,6) == 10
  
 def test_subtract():
   assert subtract(4,6) == -2
   
-def test_mult():
-  assert mult(4,6) == 24
+def test_multiply():
+  assert multiply(4,6) == 24
   
 def test_divide():
   assert divide(42,6) == 7
